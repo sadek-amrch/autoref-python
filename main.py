730ab02a8d91b99c4dd3764ffa124b8c148cf993
Adda Ismaël (HES) <ismael.adda@hes.adhes.hesge.ch>
2020-09-18 18:19:32 +0200

    cours

---
+++ main.py
@@ -0,0 +1,8 @@
+from link import Link
+import sys
+
+array_link = None
+
+print("***********************Autoref by Sadek, Adamo, Ismael**********************************")
+file_link = input("Merci d'entrer le nom du fichier : ")
+
