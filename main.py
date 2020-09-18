86ff015c426735b9cb5e8aa3671825e4a2cd6665
Adda Ismaël (HES) <ismael.adda@hes.adhes.hesge.ch>
2020-09-18 18:36:53 +0200

    ddd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from link import Link
+from FileManager import FileManager
 import sys
 
 array_link = None
@@ -6,3 +7,10 @@ array_link = None
 print("***********************Autoref by Sadek, Adamo, Ismael**********************************")
 file_link = input("Merci d'entrer le nom du fichier : ")
 
+fm:FileManager = FileManager(file_link, 'test.txt')
+
+array_link = (fm.read_file())
+
+for f in array_link:
+    link:Link = Link(f,"t", "t")
+    link.PrintLink()
