f69c3212fd87549eef221e345daca5f7c3533245
Sadek Amrouche <sadek@amrouche.ch>
2020-09-18 18:18:28 +0200

    Curl Basis

--- curl.py
+++ curl.py
@@ -1,2 +1,14 @@
-import pycurl
-from io import BytesIO
+import requests
+
+class curl:
+    def __init__(self, adress):
+        self.adress: str = adress
+        self.timeout: int = 3000
+
+
+    def get(self):
+        req = requests.get(self.adress)
+        return req.text;
+
+    def filter(self):
+        
