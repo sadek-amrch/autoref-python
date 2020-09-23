b0d470f2ef6682d5cb29a6bd0e8e566f2ffdc480
Adamo-C <71258866+Adamo-C@users.noreply.github.com>
2020-09-23 08:20:10 +0200

    Update FileManager.py

--- FileManager.py
+++ FileManager.py
@@ -1,12 +1,12 @@
 class FileManager:
 
-    def __init__(self, input_file_path, output_file_path):
-        self.input_file_path = input_file_path
-        self.output_file_path = output_file_path
+    def __init__(self, input_file_path: str, output_file_path: str):
+        self.input_file_path: str = input_file_path
+        self.output_file_path: str = output_file_path
         self.links_list = None
 
     def read_file(self):
         with open(self.input_file_path) as f:
-            self.links_list = f.readlines()
+            self.links_list: list = f.readlines()
             self.links_list = [x.strip() for x in self.links_list]
             return self.links_list
